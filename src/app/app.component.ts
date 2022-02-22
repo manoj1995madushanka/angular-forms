d6112e9423741f81a818fa8ec68d9991273501b9
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-22 22:00:08 +0530

    add ngSubmit trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  /**
+   * trigger when user click submit button
+   * */
+  onSubmit() {
+    console.log('user clicked submit.');
   }
 }
